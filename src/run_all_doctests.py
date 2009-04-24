4e5037fa80fff40c5f3c86b4146c8a6086532fd6
hltbra <hltbra@c4cc6486-30dc-11de-874f-bb9810f3bc79>
2009-04-24 19:25:37 +0000

    limiting doctests to .txt

--- src/run_all_doctests.py
+++ src/run_all_doctests.py
@@ -5,5 +5,7 @@ import os
 if __name__ == '__main__':
     doctests_path = os.path.join(os.path.dirname(__file__), 'doctests')
     for doctest_file in os.listdir(doctests_path):
+        if not doctest_file.endswith('.txt'):
+            continue
         doctest.testfile(os.path.join(doctests_path, doctest_file),
                          optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
