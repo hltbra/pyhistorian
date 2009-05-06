528bda2f0cbc37205fb5e8d68a9997b35346b0d0
hltbra <hltbra@c4cc6486-30dc-11de-874f-bb9810f3bc79>
2009-05-06 15:56:18 +0000

    added language.py to doctests runner

--- src/run_all_doctests.py
+++ src/run_all_doctests.py
@@ -3,6 +3,7 @@
 
 import doctest
 import os
+import language
 
 if __name__ == '__main__':
     doctests_path = os.path.join(os.path.dirname(__file__), 'doctests')
@@ -11,3 +12,5 @@ if __name__ == '__main__':
             continue
         doctest.testfile(os.path.join(doctests_path, doctest_file),
                          optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+    doctest.testmod(language,
+                    optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
