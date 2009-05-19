04f0e0a5cb56ec7e04ed74bc1004c5a8544adc87
hltbra <hltbra@c4cc6486-30dc-11de-874f-bb9810f3bc79>
2009-05-19 13:55:08 +0000

    fixed issue 4- I dont know why doctests print use different output (print and sys.stdout.write are different?!?!), should be reviewed!

--- src/run_all_doctests.py
+++ src/run_all_doctests.py
@@ -6,14 +6,18 @@ import os
 import language
 import suite
 
+FLAGS = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
+
 if __name__ == '__main__':
-    doctests_path = os.path.join(os.path.dirname(__file__), 'doctests')
+    doctests_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+    'doctests'))
     for doctest_file in os.listdir(doctests_path):
-        if not doctest_file.endswith('.txt'):
-            continue
-        doctest.testfile(os.path.join(doctests_path, doctest_file),
-                         optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+       if not doctest_file.endswith('.txt'):
+           continue
+       doctest.testfile(os.path.join(doctests_path, doctest_file),
+                        optionflags=FLAGS,
+                        module_relative=False)
 
     for module in [language, suite]:
         doctest.testmod(module,
-                    optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+                    optionflags=FLAGS,)
