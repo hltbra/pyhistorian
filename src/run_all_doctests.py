8b3604e599cee730a5edb121c59dd9f0430f27f8
Hugo Lopes Tavares <hltbra@gmail.com>
2009-05-30 21:42:42 -0300

    replaced doctests directory running call to tests directory ones

--- src/run_all_doctests.py
+++ src/run_all_doctests.py
@@ -5,42 +5,12 @@ import doctest
 import os
 import language
 import suite
-import pyhistorian
-import should_dsl
-from cStringIO import StringIO
-
-pyhistorian_globs = {'Scenario': pyhistorian.Scenario,
-                     'Story': pyhistorian.Story,
-                     'Step': pyhistorian.Step,
-                     'Given': pyhistorian.Given,
-                     'When': pyhistorian.When, 
-                     'Then': pyhistorian.Then,
-                     'DadoQue': pyhistorian.DadoQue,
-                     'Quando': pyhistorian.Quando,
-                     'Entao': pyhistorian.Entao,}
-
-should_dsl_globs = { 'should_be': should_dsl.should_be, 
-                     'should_not_be': should_dsl.should_not_be,
-                     'should_have': should_dsl.should_have,
-                     'should_not_have': should_dsl.should_not_have, }
-
-all_globs = pyhistorian_globs
-all_globs.update(should_dsl_globs)
-all_globs.update({'StringIO': StringIO,})
+from tests import run_tests
 
 FLAGS = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
 
 if __name__ == '__main__':
-    doctests_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-    'doctests'))
-    for doctest_file in os.listdir(doctests_path):
-       if not doctest_file.endswith('.txt'):
-           continue
-       doctest.testfile(os.path.join(doctests_path, doctest_file),
-                        optionflags=FLAGS,
-                        module_relative=False,
-                        globs=all_globs)
-
+    run_tests()
     for module in [language, suite]:
         doctest.testmod(module,
                     optionflags=FLAGS,)
