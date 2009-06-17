036f50da9b674eb50453a3cb2f70657c1b2a7a40
Hugo Lopes Tavares <hltbra@gmail.com>
2009-06-17 14:13:43 -0300

    github is the official `url` link; added classifiers; the `long_description` is the README.rst

--- setup.py
+++ setup.py
@@ -2,16 +2,29 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '0.5dev'
+readme_fd = open('README.rst')
+readme = readme_fd.read()
+readme_fd.close()
+
 
 setup(name='pyhistorian',
       version=version,
-      description="pyhistorian is a tool for writing specifications using Given-When-Then template",
-      long_description="",
-      classifiers=[],
+      description="pyhistorian is a BDD tool for writing specifications using Given-When-Then template",
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          'Topic :: Software Development :: Testing',
+      ],
       keywords='bdd dsl python',
       author='Hugo Lopes Tavares',
       author_email='hltbra@gmail.com',
-      url='http://code.google.com/p/pyhistorian/',
+      url='http://github.com/hugobr/pyhistorian',
       license='MIT License',
       packages=['pyhistorian'],
       package_dir={'pyhistorian': 'src'},
