934dfe8637a354e0e5958b798c1e2a469bcc148f
Hugo Lopes Tavares <hltbra@gmail.com>
2009-07-09 17:35:43 -0300

    updated story text to something makes more sense

--- src/tests/story.py
+++ src/tests/story.py
@@ -1,17 +1,16 @@
 '''
 >>> story.run()
 >>> print output.getvalue()
-Story: Hacker Story
-As a hacker
-I want to hack it
-So that it is hacked!
+Story: Faked Story
+As a fake
+I want to run a simple story
+So that it runs sucessfully and give me a good output
 <BLANKLINE>
 Scenario 1: Fake Title
   Given I run it   ... OK
   When I type X   ... OK
   Then it shows me X   ... OK
 <BLANKLINE>
-
 '''
 
 from pyhistorian import Story
@@ -22,7 +21,8 @@ class FakeScenario(object):
     _givens = _whens = _thens = []
     title = 'Fake Title'
 
-    def set_story(self, story): pass
+    def set_story(self, story):
+        """default interface (should do nothing)"""
 
     def run(self):
         output.write('  Given I run it   ... OK\n')
@@ -30,10 +30,10 @@ class FakeScenario(object):
         output.write('  Then it shows me X   ... OK\n')
 fake_scenario = FakeScenario()
 
-story = Story(title='Hacker Story',
-              as_a='hacker',
-              i_want_to='hack it',
-              so_that='it is hacked!',
+story = Story(title='Faked Story',
+              as_a='fake',
+              i_want_to='run a simple story',
+              so_that='it runs sucessfully and give me a good output',
               output=output)
 story.add_scenario(fake_scenario)
 
