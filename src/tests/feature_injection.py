804dd87e43dd25f3c5040e296cdd4dfc5c52f1b4
Hugo Lopes Tavares <hltbra@gmail.com>
2009-08-10 23:37:48 -0300

    fixed tests

--- src/tests/feature_injection.py
+++ src/tests/feature_injection.py
@@ -3,12 +3,12 @@
     >>> print output.getvalue()
     Story: Using in order to
     In order to avoid hard understandable stories
-    As a business analyst
+    As a story writer
     I want to be able to write "In order to/As a/I want to"
     <BLANKLINE>
     Scenario 1: Empty scenario
     <BLANKLINE>
-    Ran 1 scenario with 0 failures, 0 erros and 0 steps pending
+    Ran 1 scenario with 0 failures, 0 errors and 0 steps pending
     <BLANKLINE>
 
     >>> UsingInOrderTo.run()
