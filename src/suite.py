3d9e74066f9cb3412c96f7887e9a71d0710b5974
Hugo Lopes Tavares <hltbra@gmail.com>
2010-09-01 11:25:27 -0300

    it can be run as plone3 test suite. need to improve output

--- src/suite.py
+++ src/suite.py
@@ -31,7 +31,7 @@ class _Failure(object):
         return ''
 
 
-class _StoryCase(object):
+class _StoryCase(unittest.TestCase):
     def __init__(self, story):
         self._story = story
         self._steps = []
@@ -42,25 +42,128 @@ class _StoryCase(object):
             for method, message, args in scenario._givens + scenario._whens + scenario._thens:
                 self._steps.append(getattr(scenario, method.func_name))
 
-    def run_steps(self, result):
+    def run(self, result):
         for step in self._steps:
-            result.startTest(step)
+            result.startTest(self)
             try:
                 step()
-                result.addSuccess(step)
+                result.addSuccess(self)
             except AssertionError, e:
                 result.addFailure(_Failure(e), sys.exc_info())
             except Exception, e:
                 result.addError(_Failure(e), sys.exc_info())
 
+    def __str__(self):
+        return self._story._title
+#        return "%s (%s)" % ('run', unittest._strclass(self.__class__))
 
-class PyhistorianSuite(object):
+    def __repr__(self):
+        return "<%s testMethod=%s>" % \
+               (_strclass(self.__class__), 'run')
+
+
+#    failureException = AssertionError
+#
+#    def __init__(self, methodName='runTest'):
+#        """Create an instance of the class that will use the named test
+#           method when executed. Raises a ValueError if the instance does
+#           not have a method with the specified name.
+#        """
+#        try:
+#            self.__testMethodName = methodName
+#            testMethod = getattr(self, methodName)
+#            self.__testMethodDoc = testMethod.__doc__
+#        except AttributeError:
+#            raise ValueError, "no such test method in %s: %s" % \
+#                  (self.__class__, methodName)
+#
+#    def setUp(self):
+#        "Hook method for setting up the test fixture before exercising it."
+#        pass
+#
+#    def tearDown(self):
+#        "Hook method for deconstructing the test fixture after testing it."
+#        pass
+#
+#    def countTestCases(self):
+#        return 1
+#
+#    def defaultTestResult(self):
+#        return TestResult()
+#
+#    def shortDescription(self):
+#        """Returns a one-line description of the test, or None if no
+#        description has been provided.
+#
+#        The default implementation of this method returns the first line of
+#        the specified test method's docstring.
+#        """
+#        doc = self.__testMethodDoc
+#        return doc and doc.split("\n")[0].strip() or None
+#
+#    def id(self):
+#        return "%s.%s" % (_strclass(self.__class__), self.__testMethodName)
+#
+#    def __str__(self):
+#        return "%s (%s)" % (self.__testMethodName, _strclass(self.__class__))
+#
+#    def __repr__(self):
+#        return "<%s testMethod=%s>" % \
+#               (_strclass(self.__class__), self.__testMethodName)
+#
+#    def run(self, result=None):
+#        if result is None: result = self.defaultTestResult()
+#        result.startTest(self)
+#        testMethod = getattr(self, self.__testMethodName)
+#        try:
+#            try:
+#                self.setUp()
+#            except KeyboardInterrupt:
+#                raise
+#            except:
+#                result.addError(self, self.__exc_info())
+#                return
+#
+#            ok = False
+#            try:
+#                testMethod()
+#                ok = True
+#            except self.failureException:
+#                result.addFailure(self, self.__exc_info())
+#            except KeyboardInterrupt:
+#                raise
+#            except:
+#                result.addError(self, self.__exc_info())
+#
+#            try:
+#                self.tearDown()
+#            except KeyboardInterrupt:
+#                raise
+#            except:
+#                result.addError(self, self.__exc_info())
+#                ok = False
+#            if ok: result.addSuccess(self)
+#        finally:
+#            result.stopTest(self)
+#
+#    def __call__(self, *args, **kwds):
+#        return self.run(*args, **kwds)
+#
+#    def debug(self):
+#        """Run the test without collecting errors in a TestResult"""
+#        self.setUp()
+#        getattr(self, self.__testMethodName)()
+#        self.tearDown()
+#
+
+class PyhistorianSuite(unittest.TestSuite):
     def __init__(self, *stories):
         self._story_cases = [_StoryCase(story) for story in stories]
+        self._tests = self._story_cases
 
     def __call__(self, result):
         for story in self._story_cases:
-            story.run_steps(result)
+            story.run(result)
 
 
 if __name__ == '__main__':
